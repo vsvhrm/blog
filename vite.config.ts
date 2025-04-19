5bb81d9467a74554feb13d4c5159ab43065c2d16
Vsevolod <vsevolod.khramtsov@gmail.com>
2025-04-19 16:51:40 +0500

    feat: add svg spritemap plugin for icon management

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,21 @@
+import svgSpritemap from '@spiriit/vite-plugin-svg-spritemap';
 import vue from '@vitejs/plugin-vue';
 import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    svgSpritemap('./src/assets/icons/*.svg', {
+      route: '__icons',
+      prefix: false,
+      output: {
+        filename: 'icons.[hash][extname]',
+        name: 'icons.svg',
+        view: false
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
